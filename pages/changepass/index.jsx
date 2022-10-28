382aac51d425e770769c64393633836bead83759
maderisza <maderisza@gmail.com>
2022-10-29 01:01:29 +0700

    add:pages changepass

---
+++ pages/changepass/index.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Header from "components/header";
+import Aside from "components/aside";
+import Footer from "components/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="main container-fluid py-2 bg-light rounded">
+        <div className="row gap-2">
+          <div className="left_home col col-md-2">
+            <Aside />
+          </div>
+          <div className="right_home col col-md-9 bg-white rounded text-center">
+            <div className="Avatar mt-3">
+              <img src="avatar.png" alt="" style={{ width: 50, height: 50 }} />
+            </div>
+            <div className="edit_avatar mt-2">
+              <h6>Edit</h6>
+            </div>
+            <div className="number_avatar">
+              <h6>+62-81244-878919</h6>
+            </div>
+            <div className="group_form justify-content-center align-items-center">
+              <form>
+                <div className="omrs-input-group mt-3">
+                  <label className="omrs-input-underlined">
+                    <input type="email" name="email" required />
+                    <span className="omrs-input-label">Enter your e-mail</span>
+                    <span className="omrs-input-helper"></span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-envelope"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                    </svg>
+                  </label>
+                </div>
+                <div className="omrs-input-group mt-3 ml-5">
+                  <label className="omrs-input-underlined">
+                    <input name="password" required />
+                    <span className="omrs-input-label">
+                      Enter your password
+                    </span>
+                    <span className="omrs-input-helper"></span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-lock"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
+                    </svg>
+                    <label className="omrs-input-underlined2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-eye-slash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                        <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                        <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
+                      </svg>
+                    </label>
+                  </label>
+                </div>
+              </form>
+            </div>
+            {/* end card top */}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
