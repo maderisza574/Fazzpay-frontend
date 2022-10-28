312e4740dbddc9e5f40e74f412fe0b27fe2adea0
maderisza <maderisza@gmail.com>
2022-10-28 21:32:14 +0700

    add:some detail and integration

--- pages/signin/index.jsx
+++ pages/signin/index.jsx
@@ -3,10 +3,12 @@ import axiosClient from "utils/axios";
 
 import Cookies from "js-cookie";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Signin() {
   const router = useRouter();
   const [form, setForm] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async () => {
     try {
@@ -25,6 +27,9 @@ export default function Signin() {
   const handleChangeText = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+  // const handleShowPassword = () => {
+  //   setShowPassword(!showPassword);
+  // };
   return (
     <div>
       <div className="contsign container-fluid py-0 text center">
@@ -69,6 +74,7 @@ export default function Signin() {
                         type="email"
                         name="email"
                         onChange={handleChangeText}
+                        required
                       />
                       <span class="omrs-input-label">Enter your e-mail</span>
                       <span class="omrs-input-helper"></span>
@@ -87,9 +93,10 @@ export default function Signin() {
                   <div class="omrs-input-group mt-3">
                     <label class="omrs-input-underlined">
                       <input
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         name="password"
                         onChange={handleChangeText}
+                        required
                       />
                       <span class="omrs-input-label">Enter your password</span>
                       <span class="omrs-input-helper"></span>
@@ -103,19 +110,36 @@ export default function Signin() {
                       >
                         <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
                       </svg>
-                      <label className="omrs-input-underlined2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-eye-slash"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
-                          <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
-                          <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
-                        </svg>
+                      <label
+                        className="omrs-input-underlined2"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-eye-slash"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                            <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                            <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-eye"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                          </svg>
+                        )}
                       </label>
                     </label>
                   </div>
@@ -123,7 +147,9 @@ export default function Signin() {
                 {/* end form */}
               </div>
             </div>
-            <div className="forgot_button  mt-1">Forgot Password?</div>
+            <div className="forgot_button  mt-1">
+              <Link href="/resetpass">Forgot Password?</Link>
+            </div>
             <div className="login_button mt-3">
               <button
                 className="btn btn-primary w-75"
@@ -134,7 +160,7 @@ export default function Signin() {
               </button>
             </div>
             <div className="nav_signup mt-3">
-              Dont have an account? Lets Sign Up
+              Dont have an account? Lets <Link href="/signup"> Sign Up</Link>
             </div>
           </div>
         </div>
